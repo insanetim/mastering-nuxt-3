3b9f86a3c6a615a022b89eb803b3f1ce7395d653
insanetim <47120724+insanetim@users.noreply.github.com>
2023-11-01 13:17:37 +0200

    update

--- server/middleware/user.ts
+++ server/middleware/user.ts
@@ -3,7 +3,9 @@ import { serverSupabaseUser } from '#supabase/server'
 export default defineEventHandler(async event => {
   try {
     const user = await serverSupabaseUser(event)
-    event.context.user = user
+    if (user) {
+      event.context.user = user
+    }
   } catch (error) {
     console.error(error)
   }
